3844329da5b3f83344ca3e2d08c3bd6eaaf2c515
zhl <zhanglin@vdongchina.com>
2020-11-28 11:08:18 +0800

    go group

--- gee/gee/gee.go
+++ gee/gee/gee.go
@@ -1,36 +1,62 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
+
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	parent      *RouterGroup
+	engine      *Engine
+}
 
 type HandlerFunc func(*Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
 }
 
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (e *Engine) addRouter(method, pattern string, handler HandlerFunc) {
-	e.router.addRoute(method, pattern, handler)
+func (g *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := g.engine
+	newGroup := &RouterGroup{
+		prefix: g.prefix + prefix,
+		parent: g,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
 
-func (e *Engine) GET(pattern string, handler HandlerFunc) {
-	e.addRouter("GET", pattern, handler)
+func (g *RouterGroup) addRouter(method, comp string, handler HandlerFunc) {
+	pattern := g.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	g.engine.router.addRoute(method, pattern, handler)
 }
 
-func (e *Engine) POST(pattern string, handler HandlerFunc) {
-	e.addRouter("POST", pattern, handler)
+func (g *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	g.addRouter("GET", pattern, handler)
 }
 
-func (e *Engine) Run(addr string) (err error) {
-	return http.ListenAndServe(addr, e)
+func (g *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	g.addRouter("POST", pattern, handler)
 }
 
-func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (g *RouterGroup) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, g.engine)
+}
+
+func (g *RouterGroup) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
-	e.router.handle(c)
+	g.engine.router.handle(c)
 }
